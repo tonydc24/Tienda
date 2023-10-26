0e47554651304c0479732f365503802a1989c2dd
Anthony <Anthony@Tony>
2023-10-25 22:49:22 -0600

    6.0

---
+++ src/main/java/com/tienda/service/FirebaseStorageService.java
@@ -0,0 +1,22 @@
+package com.tienda.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public interface FirebaseStorageService {
+
+    public String cargaImagen(MultipartFile archivoLocalCliente, String carpeta, Long id);
+
+    //El BuketName es el <id_del_proyecto> + ".appspot.com#
+    final String BucketName = "techshop-70ab2.appspot.com";
+
+    //Esta es la ruta básica de este proyecto Techshop
+    final String rutaSuperiorStorage = "techshop";
+
+    //Ubicación donde se encuentra el archivo de configuración Json
+    final String rutaJsonFile = "firebase";
+    
+    //El nombre del archivo Json
+    final String archivoJsonFile = "techshop-70ab2-firebase-adminsdk-thi93-f137221b4c.json";
+}
--- src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
+++ src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
@@ -1,31 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.tienda.service.impl;
 
-import com.tienda.dao.CategoriaDao;
 import com.tienda.domain.Categoria;
 import com.tienda.service.CategoriaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.tienda.dao.CategoriaDao;
 
 @Service
 public class CategoriaServiceImpl implements CategoriaService {
-   
+
     @Autowired
     private CategoriaDao categoriaDao;
-    
-    
+
     @Override
-    public List <Categoria > getCategorias (boolean activos ){
-    
-     var lista = categoriaDao.findAll();
+    @Transactional(readOnly = true)
+    public List<Categoria> getCategorias(boolean activos) {
+        var lista = categoriaDao.findAll();
         if (activos) {
-            lista.removeIf(e-> !e.isActivo());
-            
+            lista.removeIf(e -> !e.isActivo());
         }
         return lista;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Categoria getCategoria(Categoria categoria) {
+        return categoriaDao.findById(categoria.getIdCategoria()).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void save(Categoria categoria) {
+        categoriaDao.save(categoria);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Categoria categoria) {
+        categoriaDao.delete(categoria);
     }
 }
